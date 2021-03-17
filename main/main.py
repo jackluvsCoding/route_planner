7f3c14853b9e039c4f8f46ed8baae35e60d20226
Jack <jackhysell@gmail.com>
2021-03-17 12:37:16 -0500

    Secrets

--- main/main.py
+++ main/main.py
@@ -1,16 +1,15 @@
 import requests
+from secrets import config
 
-base_url = "https://maps.googleapis.com/maps/api/distancematrix/json"
-api_key = 'AIzaSyD-1I5hn3CwJj8fI2Cgr9h7YTIzz-47qs8'
 
 
 def main():
     gcp_response = requests.get(
-        base_url,
+        config.base_url,
         params=[('units', 'imperial'),
                 ('origins', '7700 Sunwood Dr. Nw, Ramsey, MN, USA'),
                 ('destinations', '4308 Village Green Ct., Sioux City, IA, USA'),
-                ('key', api_key)]
+                ('key', config.api_key)]
     )
     print(gcp_response.url)
     print('\n')
---
+++ secrets/config.py
@@ -0,0 +1,2 @@
+base_url = "https://maps.googleapis.com/maps/api/distancematrix/json"
+api_key = 'AIzaSyD-1I5hn3CwJj8fI2Cgr9h7YTIzz-47qs8'
